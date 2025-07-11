0339e68d3d26307b0aa3d00d81facfdbbac53a8c
JahidulIslam <ovi242-35-019@diu.edu.bd>
2025-07-12 00:20:22 +0600

    Update doctor.c

--- doctor.c
+++ doctor.c
@@ -1,109 +1,180 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include "doctor.h"
+#define FILE_PATH "data/doctor.txt"
+
+void doctorMenu()
+{
+    int choice;
+    do
+    {
+        printf("\n-----DOCTOR MANAGEMENT-----\n");
+        printf("1. Add doctor\n");
+        printf("2. View doctor(s)\n");
+        printf("3. Search doctor by ID\n");
+        printf("4. Delete doctor\n");
+        printf("5. Return to main menu\n");
+        printf("Enter a choice: ");
+        scanf("%d", &choice);
+        switch(choice)
+        {
+            case 1:
+                addDoctor();
+                break;
+            case 2:
+                viewDoctor();
+                break;
+            case 3:
+                searchDoctor();
+                break;
+            case 4:
+                deleteDoctor();
+                break;
+            case 5:
+                printf("Returning to main menu\n");
+                break;
+            default:
+                printf("Invalid choice. Try again...\n");
+        }
+    } while (choice != 5);
+}
 
 void addDoctor()
 {
-    doctor D;
-
-    printf("Type the doctors name:\n");
-    fgets(D.name,sizeof(D.name),stdin);   /// Name input
-    D.name[strcspn(D.name,"\n")] = '\0';  /// Removing newline
-
-    printf("Type the doctors ID:\n");
-    scanf("%d",&D.id);                    //ID input
-
-    printf("Type the doctors age:\n");
-    scanf("%d",&D.age);                    /// Age input
-
-    printf("Type the doctors gender: M or F\n");
-    scanf(" %c",&D.gender);                 /// Gender input. Space for before %c to ensure safety
-
-    while((getchar())!='\n');           /// Clearing Input buffer 
-
-    printf("Type the doctors specializations:\n");
-    fgets(D.speciality,sizeof(D.speciality),stdin); /// Diagnosis input
-    D.speciality[strcspn(D.speciality,"\n")] = '\0'; /// Removing newline
-    
-    printf("Type experience:\n");
-    scanf("%d",&D.experience);          /// Input experience
-
-    FILE *ptr = fopen("Doctors.dat","ab");
-    if(ptr==NULL)
+    FILE *fp = fopen(FILE_PATH, "a");
+    if (fp == NULL)
     {
-        printf("File couldn't open;\n");
+        printf("Failed to open file\n");
         return;
     }
+    Doctor D;
 
-    fwrite(&D,sizeof(doctor),1,ptr);
-    fclose(ptr);
+    printf("Enter doctor's id: ");
+    scanf("%d", &D.id);
+    getchar();
 
-    printf("Doctor added succesfully:\n");
+    printf("Enter name: ");
+    fgets(D.name, sizeof(D.name), stdin);
+    D.name[strcspn(D.name, "\n")] = '\0';
+
+    printf("Enter age: ");
+    scanf("%d", &D.age);
+    getchar();
+
+    printf("Enter gender: ");
+    fgets(D.gender, sizeof(D.gender), stdin);
+    D.gender[strcspn(D.gender, "\n")] = '\0';
+
+    printf("Enter specialty: ");
+    fgets(D.specialty, sizeof(D.specialty), stdin);
+    D.specialty[strcspn(D.specialty, "\n")] = '\0';
+
+    fprintf(fp, "%d|%s|%d|%s|%s\n", D.id, D.name, D.age, D.gender, D.specialty);
+    fclose(fp);
+    printf("Doctor added successfully!\n");
 }
 
-void viewDoctors()
+void viewDoctor()
 {
-    FILE *ptr=fopen("Doctors.dat","rb");
-    if(ptr==NULL)
+    FILE *fp = fopen(FILE_PATH, "r");
+    if (fp == NULL)
     {
-        printf("No doctor records found\n");
+        printf("Failed to open file\n");
         return;
     }
+    Doctor D;
 
-    doctor D;
-    int count = 1;
-    while(fread(&D,sizeof(doctor),1,ptr))
+    printf("\n-----DOCTOR RECORDS-----\n");
+    while (fscanf(fp, "%d|%49[^|]|%d|%9[^|]|%49[^\n]\n", &D.id, &D.name, &D.age, &D.gender, &D.specialty) == 5)
     {
-        printf("\n---Doctor Data---\n");
-        printf("Doctor No :%d\n",count++);
-        printf("Name      : %s\n", D.name);
-        printf("ID        : %d\n", D.id);
-        printf("Age       : %d\n", D.age);
-        printf("Gender    : %c\n", D.gender);
-        printf("Speciality: %s\n", D.speciality);
-        printf("Experience:%d\n", D.experience);
-        printf("---------END---------\n");
+        printf("ID     : %d\n", D.id);
+        printf("Name   : %s\n", D.name);
+        printf("Age    : %d\n", D.age);
+        printf("Gender : %s\n", D.gender);
+        printf("Disease: %s\n", D.specialty);
+        printf("\n");
     }
-
-    fclose(ptr);
+    printf("---------END---------\n");
+    fclose(fp);
 }
 
 void searchDoctor()
 {
-    doctor D;
-    int ID,found=0;
-    printf("Type the doctor ID:\n");
-    scanf("%d",&ID);
-
-    FILE *ptr = fopen("Doctors.dat","rb");
-    if(ptr==NULL)
+    int targetID, found = 0;
+    FILE *fp = fopen(FILE_PATH, "r");
+    if (fp == NULL)
     {
-        printf("File wasnt found\n");
+        printf("Failed to open file\n");
         return;
     }
+    printf("Enter doctor's ID: ");
+    scanf("%d", &targetID);
+    Doctor D;
 
-    while(fread(&D,sizeof(doctor),1,ptr))
+    while (fscanf(fp, "%d|%49[^|]|%d|%9[^|]|%49[^\n]\n", &D.id, &D.name, &D.age, &D.gender, &D.specialty) == 5)
     {
-        if(D.id==ID)
+        if (targetID == D.id)
         {
-        found = 1;
-        printf("\n---Doctors Data---\n");
-        printf("Name      : %s\n", D.name);
-        printf("ID        : %d\n", D.id);
-        printf("Age       : %d\n", D.age);
-        printf("Gender    : %c\n", D.gender);
-        printf("Speciality: %s\n", D.speciality);
-        printf("Experience:%d\n", D.experience);
-        printf("---------END---------\n");
+            found = 1;
+
+            printf("\n-----DOCTOR INFORMATION-----\n");
+            printf("ID     : %d\n", D.id);
+            printf("Name   : %s\n", D.name);
+            printf("Age    : %d\n", D.age);
+            printf("Gender : %s\n", D.gender);
+            printf("Disease: %s\n", D.specialty);
+            printf("---------END---------\n");
         break;
         }
     }
+    fclose(fp);
 
-    fclose(ptr);
-
-    if(found == 0)
+    if (found == 0)
     {
-        printf("No doctors found with this ID\n");
+        printf("No doctor found with this ID\n");
+    }
+}
+
+void deleteDoctor()
+{
+    int targetID, found = 0;
+    FILE *fp = fopen(FILE_PATH, "r");
+    FILE *temp = fopen("data/temp.txt", "w");
+
+    if (fp == NULL || temp == NULL)
+    {
+        printf("Failed to open file\n");
+        return;
+    }
+    Doctor D;
+
+    printf("Enter doctor's id: ");
+    scanf("%d", &targetID);
+    getchar();
+
+    while (fscanf(fp, "%d|%49[^|]|%d|%9[^|]|%49[^\n]\n", &D.id, &D.name, &D.age, &D.gender, &D.specialty) == 5)
+    {
+        if (targetID == D.id)
+        {
+            found = 1;
+        }
+        else
+        {
+            fprintf(temp, "%d|%s|%d|%s|%s\n", D.id, D.name, D.age, D.gender, D.specialty);
+        }
+    }
+    fclose(fp);
+    fclose(temp);
+    remove("data/doctor.txt");
+    rename("data/temp.txt", "data/doctor.txt");
+
+    if (found == 1)
+    {
+        printf("Doctor deleted successfully!\n");
+    }
+    else
+    {
+        printf("Doctor data not found\n");
     }
 }
