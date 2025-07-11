d9cef3aa7691d29c51bb387a19f57e0617dd970a
JahidulIslam <ovi242-35-019@diu.edu.bd>
2025-07-12 00:22:49 +0600

    Update billing.h

--- billing.h
+++ billing.h
@@ -1,17 +1,18 @@
 #ifndef BILLING_H
 #define BILLING_H
 
-typedef struct billing
+typedef struct
 {
-    int patient_id;
-    double money;
-    double paid;
-    double due;
+    int billingID;
+    int patientID;
+    int appointmentID;
+    float amount;
+    char paymentStatus[20];
+} Billing;
 
-} billing;
-
-void addBilling();
-void viewBillings();
-void searchBilling();
+void addBill();
+void viewBill();
+void searchBill();
+void billingMenu();
 
 #endif
