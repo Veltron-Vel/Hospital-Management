7756b4ce812e3435ff27e57811b477447c158399
JahidulIslam <ovi242-35-019@diu.edu.bd>
2025-08-19 20:13:03 +0600

    Update patient.c

--- patient.c
+++ patient.c
@@ -1,19 +1,198 @@
-#ifndef PATIENT_H
-#define PATIENT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "patient.h"
+#define FILE_PATH "data/patient.txt"
 
-typedef struct
+void patientMenu()
 {
-    int id;
-    char name[50];
-    int age;
-    char gender[10];
-    char disease[50];
-} Patient;
+    int choice;
+    do
+    {
+        printf("\n-----PATIENT MANAGEMENT-----\n");
+        printf("1. Add patient\n");
+        printf("2. View patient(s)\n");
+        printf("3. Search patient by ID\n");
+        printf("4. Delete patient\n");
+        printf("5. Return to main menu\n");
+        printf("Enter a choice: ");
+        scanf("%d", &choice);
+        while(getchar() != '\n');
 
-void addPatient();
-void viewPatient();
-void searchPatient();
-void deletePatient();
-void patientMenu();
+        switch(choice)
+        {
+            case 1:
+                addPatient();
+                break;
+            case 2:
+                viewPatient();
+                break;
+            case 3:
+                searchPatient();
+                break;
+            case 4:
+                deletePatient();
+                break;
+            case 5:
+                printf("Returning to main menu\n");
+                break;
+            default:
+                printf("Invalid choice. Try again...\n");
+        }
+    } while (choice != 5);
+}
 
-#endif
+void addPatient()
+{
+    FILE *fp = fopen(FILE_PATH, "a");
+
+    if (fp == NULL)
+    {
+        printf("Failed to open file\n");
+        return;
+    }
+
+    Patient P;
+
+    printf("Enter patient id: ");
+    scanf("%d", &P.id);
+    while (getchar() != '\n');
+
+    printf("Enter name: ");
+    fgets(P.name, sizeof(P.name), stdin);
+    P.name[strcspn(P.name, "\n")] = '\0';
+
+    printf("Enter age: ");
+    scanf("%d", &P.age);
+    while (getchar() != '\n');
+
+    printf("Enter gender: ");
+    fgets(P.gender, sizeof(P.gender), stdin);
+    P.gender[strcspn(P.gender, "\n")] = '\0';
+
+    printf("Enter disease: ");
+    fgets(P.disease, sizeof(P.disease), stdin);
+    P.disease[strcspn(P.disease, "\n")] = '\0';
+
+    fprintf(fp, "%d|%s|%d|%s|%s\n", P.id, P.name, P.age, P.gender, P.disease);
+    fclose(fp);
+
+    printf("Patient added successfully!\n");
+}
+
+void viewPatient()
+{
+    FILE *fp = fopen(FILE_PATH, "r");
+    int count = 0;
+
+    if (fp == NULL)
+    {
+        printf("Failed to open file\n");
+        return;
+    }
+
+    Patient P;
+    printf("\n-----PATIENT RECORDS-----\n");
+
+    while (fscanf(fp, "%d|%49[^|]|%d|%9[^|]|%49[^\n]\n", &P.id, &P.name, &P.age, &P.gender, &P.disease) == 5)
+    {
+        printf("ID     : %d\n", P.id);
+        printf("Name   : %s\n", P.name);
+        printf("Age    : %d\n", P.age);
+        printf("Gender : %s\n", P.gender);
+        printf("Disease: %s\n", P.disease);
+        printf("\n");
+        count++;
+    }
+    if (count > 0)
+    {
+        printf("---------END---------\n");
+    }
+    else
+    {
+        printf("No patient information found in the records\n");
+    }
+
+    fclose(fp);
+}
+
+void searchPatient()
+{
+    int targetID, found = 0;
+    FILE *fp = fopen(FILE_PATH, "r");
+    if (fp == NULL)
+    {
+        printf("Failed to open file\n");
+        return;
+    }
+    printf("Search patient by ID: ");
+    scanf("%d", &targetID);
+    while (getchar() != '\n');
+    Patient P;
+
+    while (fscanf(fp, "%d|%49[^|]|%d|%9[^|]|%49[^\n]\n", &P.id, &P.name, &P.age, &P.gender, &P.disease) == 5)
+    {
+        if (targetID == P.id)
+        {
+            found = 1;
+
+            printf("\n-----PATIENT INFORMATION-----\n");
+            printf("ID     : %d\n", P.id);
+            printf("Name   : %s\n", P.name);
+            printf("Age    : %d\n", P.age);
+            printf("Gender : %s\n", P.gender);
+            printf("Disease: %s\n", P.disease);
+            printf("---------END---------\n");
+        break;
+        }
+    }
+    fclose(fp);
+
+    if (found == 0)
+    {
+        printf("No patient found with this ID\n");
+    }
+}
+
+void deletePatient()
+{
+    int targetID, found = 0;
+    FILE *fp = fopen(FILE_PATH, "r");
+    FILE *temp = fopen("data/temp.txt", "w");
+    if (fp == NULL || temp == NULL)
+    {
+        printf("Failed to open file\n");
+        return;
+    }
+
+    printf("Enter patient's ID: ");
+    scanf("%d", &targetID);
+    while (getchar() != '\n');
+    Patient P;
+
+    while (fscanf(fp, "%d|%49[^|]|%d|%9[^|]|%49[^\n]\n", &P.id, &P.name, &P.age, &P.gender, &P.disease) == 5)
+    {
+        if (targetID == P.id)
+        {
+            found = 1;
+        }
+        else
+        {
+            fprintf(temp, "%d|%s|%d|%s|%s\n", P.id, P.name, P.age, P.gender, P.disease);
+        }
+    }
+
+    fclose(fp);
+    fclose(temp);
+    remove("data/patient.txt");
+    rename("data/temp.txt", "data/patient.txt");
+
+    if (found == 1)
+    {
+        printf("Patient deleted successfully!\n");
+    }
+    else
+    {
+        printf("Patient data not found\n");
+    }
+}
