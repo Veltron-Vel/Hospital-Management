51d107794fe02a61df89953e4f135be9f8ad65c0
JahidulIslam <ovi242-35-019@diu.edu.bd>
2025-08-20 01:17:25 +0600

    Add files via upload

--- main.c
+++ main.c
@@ -17,17 +17,17 @@ int main()
 
     if (access == 1)
     {
-        printf("\nLogin successful. Welcome to Hospital Management System!\n");
+        printf("\n\033[32mLogin successful. Welcome to Hospital Management System!\033[0m\n");
         showAdminMenu();
     }
     else if (access == -1)
     {
-        printf("\nInvalid password. Access denied");
+        printf("\n\033[31mInvalid password. Access denied\033[0m\n");
         return -1;
     }
     else
     {
-        printf("\nWelcome to Hospital Management System!\n");
+        printf("\n\033[32mWelcome to Hospital Management System!\033[0m\n");
         showUserMenu();
     }
     return 0;
@@ -37,7 +37,7 @@ int login()
 {
     int user;
     char password[20];
-    printf("-----LOGIN-----\n");
+    printf("\033[1;34m-----LOGIN-----\033[0m\n");
     printf("Log in as:\n");
     printf("1. Admin\n");
     printf("2. User\n");
@@ -69,7 +69,7 @@ void showAdminMenu()
     int choice;
     do
     {
-        printf("\n-----HOSPITAL MANAGEMENT SYSTEM-----\n");
+        printf("\n\033[1;34m-----HOSPITAL MANAGEMENT SYSTEM-----\033[0m\n");
         printf("1. Patient Management\n");
         printf("2. Doctor management\n");
         printf("3. Appointment\n");
@@ -94,10 +94,10 @@ void showAdminMenu()
                 billingMenu();
                 break;
             case 5:
-                printf("Exiting system...\n");
+                printf("\033[34mExiting system...\033[0m\n");
                 break;
             default:
-                printf("Invalid choice. Try again...\n");
+                printf("\033[31mInvalid choice. Try again...\033[0m\n");
         }
     } while (choice != 5);
 }
@@ -107,7 +107,7 @@ void showUserMenu()
     int choice;
     do
     {
-        printf("\n-----HOSPITAL MANAGEMENT SYSTEM-----\n");
+        printf("\n\033[1;34m-----HOSPITAL MANAGEMENT SYSTEM-----\033[0m\n");
         printf("1. Search patient by ID\n");
         printf("2. View available doctors\n");
         printf("3. Search appointment by ID\n");
@@ -132,10 +132,10 @@ void showUserMenu()
                 searchBill();
                 break;
             case 5:
-                printf("Exiting system...\n");
+                printf("\033[34mExiting system...\033[0m\n");
                 break;
             default:
-                printf("Invalid choice. Try again...\n");
+                printf("\033[31mInvalid choice. Try again...\033[0m\n");
         }
     } while (choice != 5);
 }
